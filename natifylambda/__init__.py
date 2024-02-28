b4484dbf0c47f281c4c14e003c7cd29d94f6b2c7
Prem Rara <p@rara.dev>
2024-02-28 15:40:33 -0500

    Bump version: 0.1.20 → 0.1.21

--- natifylambda/__init__.py
+++ natifylambda/__init__.py
@@ -5,4 +5,4 @@ natifyLambda: As in deploy a NAT Instance and edit private subnets to use it.
 
 __author__ = """Prem Rara"""
 __email__ = 'p@rara.dev'
-__version__ = '0.1.20'
+__version__ = '0.1.21'
